79326fdb91dbac7066c30679a5c4b833c091cc4a
vovaf88 <vovaf88@mail.ru>
2024-04-03 23:22:03 +0300

    add buy products

--- erp/erp/src/views/ProductList.vue
+++ erp/erp/src/views/ProductList.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
         <h3>Products</h3>
+        <label
+            v-for="category in listCategory"
+            :key="category.id"
+        >
+            <input @change="loadFilterListProduct" v-model="selectedCategory" :value="category.name" type="radio">
+            <span>{{ category.name }}</span>
+            
+        </label>
+        <p>{{ selectedCategory  }} <button @click="loadListProduct">Сброс</button> </p>
+        <hr>
         <div>
             <router-link to="/products/create">Создать номенклатуру</router-link>/
         </div>
@@ -20,19 +30,37 @@ export default {
     name: "ProductList",
     data() {
         return {
-            listProduct: []
+            listProduct: [],
+            listCategory: [],
+            selectedCategory: "",
         }
     },
     components: {},
     created(){
-        this.loadListProduct()
+        this.loadListProduct();
+        this.loadListCategory();
     },
     methods: {
         async loadListProduct() {
-            this.listProduct= await fetch(
+            this.listProduct = await fetch(
                 `${this.$store.getters.getServerUrl}/products/`
+
             ).then(response => response.json())
+           
+        },
+        async loadFilterListProduct() {
+            this.listProduct = await fetch(
+                `${this.$store.getters.getServerUrl}/products/?category=${this.selectedCategory}`
+
+            ).then(response => response.json())
+            //console.log(`${this.$store.getters.getServerUrl}/products/?category=${this.selectedCategory}`)
             
+        },
+        async loadListCategory() {
+            this.listCategory = await fetch(
+                `${this.$store.getters.getServerUrl}/product-categories/`
+            ).then(response => response.json())
+          
         },
         goTo(id){
             this.$router.push({ name: 'SingleProduct', params: { id: id }})
